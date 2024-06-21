f30e72052f4ac3792804718d21b1e5a020cc0d97
Ian <ian_frye@outlook.com>
2024-06-21 06:24:41 -0400

    increasing default durtion of toast

--- hooks/useErrorToast.tsx
+++ hooks/useErrorToast.tsx
@@ -14,7 +14,7 @@ export default function useErrorToast() {
       title: title || 'Error',
       description: message,
       variant: 'destructive',
-      duration: duration || 3000,
+      duration: duration || 5000,
     });
   };
 
