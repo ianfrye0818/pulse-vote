6b1821bc4bb11ba956b7d425f4e2b55176548c9c
Ian <ian_frye@outlook.com>
2024-06-22 09:51:17 -0400

    added back-to-home linkes on auth pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,20 @@
 import { SignIn } from '@clerk/nextjs';
-import PageWrapper from '@/app/page-wrapper';
+import Link from 'next/link';
+import { ArrowLeftIcon } from 'lucide-react';
 
 export default function SignInPage() {
   return (
-    <section className='h-screen flex justify-center items-center'>
-      <SignIn />
+    <section className='h-screen flex flex-col justify-center items-center'>
+      <div className='w-max'>
+        <Link
+          href='/'
+          className='mr-auto flex gap-2 my-2'
+        >
+          <ArrowLeftIcon />
+          Back to Home
+        </Link>
+        <SignIn />
+      </div>
     </section>
   );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,20 @@
 import { SignUp } from '@clerk/nextjs';
-import PageWrapper from '@/app/page-wrapper';
+import { ArrowLeftIcon } from 'lucide-react';
+import Link from 'next/link';
 
 export default function SignUpPage() {
   return (
-    <section className='h-screen flex justify-center items-center'>
-      <SignUp />
+    <section className='h-screen flex flex-col justify-center items-center'>
+      <div className='w-max'>
+        <Link
+          href='/'
+          className='mr-auto flex gap-2 my-2'
+        >
+          <ArrowLeftIcon />
+          Back to Home
+        </Link>
+        <SignUp />
+      </div>
     </section>
   );
 }
